6315f7aed9d3c48692b2bb36f8bd8274de29e85c
zvalant <96396989+zvalant@users.noreply.github.com>
2022-02-01 22:40:09 -0600

    Add files via upload

---
+++ primecounter.py
@@ -0,0 +1,12 @@
+def prime(number):
+    prime_c = 1 # create prime counter that starts at one for the number 2
+    for current_num in range(3,number+1,2): # for loop that goes from 3-number by increments of 2
+        prime_tracker = True  # variable to keep track if number is prime/ reset tracker for new numbers
+        for num in range(3,current_num): # for loop that tracks the current number in iteration
+            if current_num % num == 0: # determines if the current number is composite
+                prime_tracker = False
+        if prime_tracker == True: # if tracker doesnt switch during iteration add 1 to prime counter
+            prime_c +=1
+    return print(prime_c)
+
+prime(100)
